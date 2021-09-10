68d290c3b26c809490eca540368c1ed262e5391d
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-09-10 13:48:06 +0100

    initial commit

---
+++ portfolio/pages/index.tsx
@@ -0,0 +1,12 @@
+
+import { Flex } from '@chakra-ui/react'
+
+
+const IndexPage = () => (
+ <Flex alignItems = "center">
+   Hi
+   <Flex height="100" background = "gray">asd</Flex>
+ </Flex>
+)
+
+export default IndexPage
