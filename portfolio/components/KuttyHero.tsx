0d542edd6ed162838d42f9e8688b0585b7957aa3
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-10-11 21:27:58 +0100

    a new file

---
+++ portfolio/components/KuttyHero.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import {
+  chakra,
+  Box,
+  useColorModeValue,
+  Button,
+  Stack,
+  Image,
+  Text,
+  Icon,
+} from "@chakra-ui/react";
+
+const KuttyHero = () => {
+  return (
+    <Box px={8} py={24} mx="auto">
+      <Box
+        w={{ base: "full", md: 11 / 12, xl: 9 / 12 }}
+        mx="auto"
+        textAlign={{ base: "left", md: "center" }}
+      >
+        <chakra.h1
+          mb={6}
+          fontSize={{ base: "4xl", md: "6xl" }}
+          fontWeight="bold"
+          lineHeight="none"
+          letterSpacing={{ base: "normal", md: "tight" }}
+          color={useColorModeValue("gray.900",'gray.100')}
+        >
+           {" "}
+          <Text
+            display={{ base: "block", lg: "inline" }}
+            w="full"
+            bgClip="text"
+            bgGradient="linear(to-r, green.400,purple.500)"
+            fontWeight="extrabold"
+          > HI THERE, I'M
+            RUI  :P.
+          </Text>{" "}
+          
+        </chakra.h1>
+        <chakra.p
+          px={{ base: 0, lg: 24 }}
+          mb={6}
+          fontSize={{ base: "lg", md: "xl" }}
+          color={useColorModeValue("gray.600",'gray.300')}
+        >
+        I like building stuff that you can see and interact with, especially interact with.
+        I'm what you may call a polymath, a jack of all trades (but still, a master of one), 
+        a know it all type of guy whose brain is a sponge. 
+        </chakra.p>
+        <Stack
+        direction={{base:"column",sm:"row"}}
+          mb={{ base: 4, md: 8 }}
+          spacing={2}
+          justifyContent={{ sm: "left", md: "center" }}
+        >
+          <Button
+            as="a"
+            variant="solid"
+            colorScheme="brand"
+            display="inline-flex"
+            alignItems="center"
+            justifyContent="center"
+            w={{ base: "full", sm: "auto" }}
+            mb={{ base: 2, sm: 0 }}
+            size="lg"
+            cursor="pointer"
+          >
+            EXPLORE RUI'S LIFE
+            <Icon boxSize={4} ml={1} viewBox="0 0 20 20" fill="currentColor">
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </Icon>
+          </Button>
+          
+        </Stack>
+      </Box>
+      <Box
+        w={{ base: "full", md: 10 / 12 }}
+        mx="auto"
+        mt={20}
+        textAlign="center"
+      >
+        <Image
+          w="full"
+          rounded="lg"
+          shadow="2xl"
+          src="https://media-exp1.licdn.com/dms/image/C4E2DAQFg8KEnZeV3OQ/profile-treasury-image-shrink_1280_1280/0/1628385373591?e=1634072400&v=beta&t=-wieGv1hradwh8-1MlN558u-wBxnmWb4L3DZzC2d4mI"
+          alt="Hellonext feedback boards software screenshot"
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default KuttyHero;
