3e2450f82ce0610515969c2864038e27528f5b9d
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-10-12 09:02:22 +0100

    solved string error

--- portfolio/pages/projects.tsx
+++ portfolio/pages/projects.tsx
@@ -9,34 +9,34 @@ const MyProjects = () => (
   <>
   <ProjectCard 
     name = "MontyCarlo" 
-    description = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem
-    modi reprehenderit vitae exercitationem aliquid dolores ullam
-    temporibus enim expedita aperiam mollitia iure consectetur dicta
-    tenetur, porro consequuntur saepe accusantium consequatur.
+    description = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem \
+    modi reprehenderit vitae exercitationem aliquid dolores ullam \
+    temporibus enim expedita aperiam mollitia iure consectetur dicta \
+    tenetur, porro consequuntur saepe accusantium consequatur. \
     "
     />
   <ProjectCard 
     name = "EasyTracer"
-    description = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem
-    modi reprehenderit vitae exercitationem aliquid dolores ullam
-    temporibus enim expedita aperiam mollitia iure consectetur dicta
-    tenetur, porro consequuntur saepe accusantium consequatur.
+    description = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem \
+    modi reprehenderit vitae exercitationem aliquid dolores ullam \
+    temporibus enim expedita aperiam mollitia iure consectetur dicta \
+    tenetur, porro consequuntur saepe accusantium consequatur. \
     "
     />
   <ProjectCard 
     name = "BrownianMotionAndroid"
-    description = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem
-    modi reprehenderit vitae exercitationem aliquid dolores ullam
-    temporibus enim expedita aperiam mollitia iure consectetur dicta
-    tenetur, porro consequuntur saepe accusantium consequatur.
+    description = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem \
+    modi reprehenderit vitae exercitationem aliquid dolores ullam \
+    temporibus enim expedita aperiam mollitia iure consectetur dicta \
+    tenetur, porro consequuntur saepe accusantium consequatur. \
     "
     />
   <ProjectCard 
     name = "JS Game Engine"
-    description = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem
-    modi reprehenderit vitae exercitationem aliquid dolores ullam
-    temporibus enim expedita aperiam mollitia iure consectetur dicta
-    tenetur, porro consequuntur saepe accusantium consequatur.
+    description = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem \
+    modi reprehenderit vitae exercitationem aliquid dolores ullam \
+    temporibus enim expedita aperiam mollitia iure consectetur dicta \
+    tenetur, porro consequuntur saepe accusantium consequatur. \
     "
     />
   </>
